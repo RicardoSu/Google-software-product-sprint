f35cc02028512abe0dd8e7caab12ccfc7e5c81aa
Ricardo <ricardolimasuarez@gmail.com>
2021-06-01 17:34:55 +0000

    header and nav menu completed

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,18 +18,38 @@
 
 
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle')
+    navClose = document.getElementById('nav-close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener("click", () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
+if(navClose){
+    navClose.addEventListener("click", () =>{
+        navMenu.classList.remove("show-menu")
+    })
+}
+
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
@@ -57,7 +77,7 @@
 
 /*==================== DARK LIGHT THEME ====================*/ 
 
- 
+
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
